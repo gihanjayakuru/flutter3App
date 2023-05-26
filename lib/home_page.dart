f74ea252b665e2274eeae5dc93e3d5dc047ef717
Gihan Jayakuru <gihan22603@gmail.com>
2023-05-26 19:32:32 +0530

    checkbox todo list

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -9,6 +9,19 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+//list of todo tasks
+  List toDoList = [
+    ["task name ", false],
+    ["task name2", false],
+  ];
+
+  //checkbox taped
+  void checkBoxChanged(bool? value, int index) {
+    setState(() {
+      toDoList[index][1] = !toDoList[index][1];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,10 +30,15 @@ class _HomePageState extends State<HomePage> {
         title: Text('TO DO'),
         elevation: 0,
       ),
-      body: ListView(
-        children: [
-          ToDoTile(),
-        ],
+      body: ListView.builder(
+        itemCount: toDoList.length,
+        itemBuilder: (context, index) {
+          return ToDoTile(
+            taskName: toDoList[index][0],
+            taskCompleted: toDoList[index][1],
+            onChanged: (value) => checkBoxChanged(value, index),
+          );
+        },
       ),
     );
   }
